54cacc95bd99ef4e7f50f43bf5e264f8c58176f1
ITSTIME <itstime0809@gmail.com>
2022-06-06 22:47:33 +0900

    Update : greedy problem failed

--- greedy/피로도.py
+++ greedy/피로도.py
@@ -16,4 +16,30 @@ for i in range(1, 25):
 
 print(work)
     
+
+
+# 두번째 풀이
     
+
+A, B, C, M = map(int, input().split())
+
+i = 1;
+piro = 0;
+piroResult = 0;
+work = 0
+# i = 1 시간 일하면 piro = +=a
+while i <= 23:
+  i+=1
+  piroResult = piro + A;
+  if  piroResult <= M:
+    piro += A;
+    work += B;
+  else:
+    if piro - C < 0:
+      work = 0
+    else:
+      piro-=C
+  if(i > 23):
+    break;
+
+print(work)
